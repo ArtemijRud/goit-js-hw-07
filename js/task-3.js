cd21cb26494226751d85a37585a482c89fce97b9
ArtemijRud <112616679+ArtemijRud@users.noreply.github.com>
2024-01-28 19:48:30 +0200

    hw-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+const userInput = document.getElementById("name-input");
+const userName = document.getElementById("name-output");
+
+const addName = () => {
+  let enterName = userInput.value.trim();
+  let upperFirstLetterEnterName = "";
+  if (enterName !== "") {
+    upperFirstLetterEnterName = enterName[0].toUpperCase() + enterName.slice(1);
+  }
+  userName.textContent =
+    upperFirstLetterEnterName === "" ? "Anonymous" : upperFirstLetterEnterName;
+  userName.style.color = "red";
+};
+
+userInput.addEventListener("input", addName);

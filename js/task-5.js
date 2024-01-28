830bbb3951f54d0ecc5d7d49912595fea98d50df
ArtemijRud <112616679+ArtemijRud@users.noreply.github.com>
2024-01-28 20:23:31 +0200

    hw-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.querySelector("body");
+
+function changeColor() {
+  let randomHex = getRandomHexColor();
+  colorSpan.textContent = randomHex;
+  body.style.backgroundColor = randomHex;
+}
+button.addEventListener("click", changeColor);
